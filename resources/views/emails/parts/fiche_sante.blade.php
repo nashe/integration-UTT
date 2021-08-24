4b7c017d0718d248d92f9a77d2405f530da99ba0
arnaud11g <arnaud11.gorce@gmail.com>
2021-08-24 12:58:07 +0200

    feat(mail template): add new mail template for health document

---
+++ resources/views/emails/parts/fiche_sante.blade.php
@@ -0,0 +1,22 @@
+<div style="background-color: #E74360; text-align: center; padding: 15px">
+  <span style="font-family:merriweather sans,helvetica neue,helvetica,arial,sans-serif; font-size: 1.3em; color: #FFFFFF">
+	<strong>La fiche Santé/Autorisation parentale</strong>
+  </span>
+</div>
+
+<div style="margin: 10px; text-align: center">
+  <span style="font-family:merriweather sans,helvetica neue,helvetica,arial,sans-serif;">
+Pour le bon déroulement de l'intégration, nous avons besoin de diverses informations à votre propos.
+Tout d'abord, quelques informations médicales en cas de problème. Ne vous inquiétez pas, ces informations seront récupérées, conservées et traitées uniquement par l'infirmerie de l'UTT.
+Ensuite, pour les étudiants mineurs, l'autorisation parentale qui sera nécessaire afin de partir pour le week-end d'intégration.
+Ces deux feuilles sont présentes ici :
+  <br>&nbsp;<br>
+  </span>
+  <a href="{{ asset('docs/autorisation.pdf') }}" target="_blank" style="background-color: #E74360; border-color: #00acd6;border-radius: 3px;color: #fff;padding: 10px 16px;text-decoration: none;font-size: 15px;line-height: 1.3333333;">Télécharge la fiche d'autorisation</a><br/>
+  <br>&nbsp;<br>
+On compte sur vous pour les ramener lors de votgre chaîne d'inscription (les horaires vous seront donnés prochainement).
+
+On se voit bientôt !
+
+La team inté
+</div>
