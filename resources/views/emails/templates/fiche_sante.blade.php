e93f20fbe0d66b6d464628dbd3ead6fe43992e8e
arnaud11g <arnaud11.gorce@gmail.com>
2021-08-24 13:16:40 +0200

    Fix errors with health document

--- resources/views/emails/templates/fiche_sante.blade.php
+++ resources/views/emails/templates/fiche_sante.blade.php
@@ -6,17 +6,15 @@
 
 <div style="margin: 10px; text-align: center">
   <span style="font-family:merriweather sans,helvetica neue,helvetica,arial,sans-serif;">
-Pour le bon déroulement de l'intégration, nous avons besoin de diverses informations à votre propos.
-Tout d'abord, quelques informations médicales en cas de problème. Ne vous inquiétez pas, ces informations seront récupérées, conservées et traitées uniquement par l'infirmerie de l'UTT.
-Ensuite, pour les étudiants mineurs, l'autorisation parentale qui sera nécessaire afin de partir pour le week-end d'intégration.
+Pour le bon déroulement de l'intégration, nous avons besoin de diverses informations à votre propos.<br>
+Tout d'abord, quelques informations médicales en cas de problème. Ne vous inquiétez pas, ces informations seront récupérées, conservées et traitées uniquement par l'infirmerie de l'UTT.<br>
+Ensuite, pour les étudiants mineurs, l'autorisation parentale qui sera nécessaire afin de partir pour le week-end d'intégration.<br>
 Ces deux feuilles sont présentes ici :
   <br>&nbsp;<br>
   </span>
   <a href="{{ asset('docs/autorisation.pdf') }}" target="_blank" style="background-color: #E74360; border-color: #00acd6;border-radius: 3px;color: #fff;padding: 10px 16px;text-decoration: none;font-size: 15px;line-height: 1.3333333;">Télécharge la fiche d'autorisation</a><br/>
   <br>&nbsp;<br>
-On compte sur vous pour les ramener lors de votgre chaîne d'inscription (les horaires vous seront donnés prochainement).
-
-On se voit bientôt !
-
+On compte sur vous pour les ramener lors de votgre chaîne d'inscription (les horaires vous seront donnés prochainement).<br>
+On se voit bientôt !<br>
 La team inté
 </div>
