7f18c093568643bff21c5829cb32a764426f90da
Noé Landré <noe.landre@laposte.net>
2022-07-18 22:14:35 +0200

    feat: remplace referral buttons

--- resources/views/Newcomers/Steps/referral.blade.php
+++ resources/views/Newcomers/Steps/referral.blade.php
@@ -29,13 +29,13 @@ La personne qui te guidera tout au long de ta vie à l'UTT
             Une fois le questionnaire rempli, nous t'enverrons dans les jours qui viennent ses coordonnées
             Mais pas d'inquiétude ! Tu as le choix de le contacter ou non : tu auras ses coordonnées mais il n'aura pas les tiennes.
         </p>
-        <p>
-            Questionnaire si tu rentres en Tronc Commun (1ère année) : <a href="https://forms.gle/QD1awJ8paPhAtGwP9">https://forms.gle/QD1awJ8paPhAtGwP9</a>
+        <div class="text-center">
+            <a href="https://forms.gle/QD1awJ8paPhAtGwP9" class="btn btn-link">Questionnaire si tu rentres en Tronc Commun (1ère année)</a>
             <br />
-            Questionnaire si tu rentres en Branche (3ème année) : <a href="https://forms.gle/V1BMMV4fSEp4ydEC8">https://forms.gle/V1BMMV4fSEp4ydEC8</a>
+            <a href="https://forms.gle/V1BMMV4fSEp4ydEC8" class="btn btn-link">Questionnaire si tu rentres en Branche (3ème année)</a>
             <br />
-            Questionnaire si tu rentres en Master : <a href="https://forms.gle/zWoYoFBawehWLEvV6">https://forms.gle/zWoYoFBawehWLEvV6</a>
-        </p>
+            <a href="https://forms.gle/zWoYoFBawehWLEvV6" class="btn btn-link">Questionnaire si tu rentres en Master</a>
+        </div>
         <div class="text-center">
             <a class="btn btn-primary" href="{{{ route('newcomer.'.Auth::user()->getNextCheck()['page']) }}}">Prochaine action à faire<br/><strong>{{{ Auth::user()->getNextCheck()['action'] }}}</strong></a>
         </div>
