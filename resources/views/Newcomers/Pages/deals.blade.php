995a6105a49621b6de8f91a340d60cefdf8b9eee
arnaud11g <arnaud11.gorce@gmail.com>
2022-08-17 14:29:06 +0200

    fix: link for soprasteria redirection

--- resources/views/Newcomers/Pages/deals.blade.php
+++ resources/views/Newcomers/Pages/deals.blade.php
@@ -49,7 +49,7 @@ Que l'intégration a trouvé pour toi !
         <p> <h4 id="soprasteria"> Sopra Steria </h4> </p>
         <p>L’un des leaders européens de la Tech reconnu pour ses activités de conseil, de services numériques et d’édition de logiciels, aide ses clients à mener leur transformation numérique et à obtenir des bénéfices concrets et durables.</p> 
         <p>Le Groupe apporte une réponse globale aux enjeux de compétitivité des grandes entreprises et organisations, en combinant une connaissance approfondie des secteurs d’activité et des technologies innovantes à une approche résolument collaborative.</p>
-        <p><a href="https://www.soprasteriarecrute.fr/">https://www.soprasteriarecrute.fr/</a></p><br/>
+        <p><a href="https://www.soprasteria.fr/carrieres">https://www.soprasteria.fr/carrieres</a></p><br/>
 
         <p><a href="https://troyes.lasergame-evolution.fr/"><img src="img/sponsors/deals/lasergame_resultat.png" style="float:right" class="img-thumbnail" /></a></p>
         <p> <h4 id="lasergame"> Laser Game Evolution </h4> </p>
