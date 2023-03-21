b80676ac86767429f4cf464e34a2256347700229
arnaud11g <arnaud11.gorce@gmail.com>
2023-03-21 22:00:07 +0100

    Remove perm respo from perms export

--- app/Http/Controllers/Admin/Exports/ExportPerms.php
+++ app/Http/Controllers/Admin/Exports/ExportPerms.php
@@ -15,6 +15,7 @@ class ExportPerms implements FromCollection, WithHeadings {
     public function collection() {
         $perms= User::select(['first_name', 'last_name', 'phone'])
         ->orderBy('start')
+        ->where('respo', 0)
         ->join('perm_users', 'users.id', '=', 'perm_users.user_id')
         ->join('perms', 'perms.id', '=', 'perm_users.perm_id')
         ->addSelect([\DB::raw('first_name'), \DB::raw('last_name'), \DB::raw('phone'), \DB::raw('perm_id'), \DB::raw('place'), \DB::raw('FROM_UNIXTIME(start) as start'), \DB::raw('FROM_UNIXTIME(end) as end'), \DB::raw('description')])
