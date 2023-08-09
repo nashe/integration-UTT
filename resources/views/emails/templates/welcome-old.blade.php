77961ccbf7fe3b8366e49265882fe5a0f9da066e
chrischidiac <christopher.chidiac@utt.fr>
2023-08-09 11:25:58 +0200

    Template Mail Cahier de vacances

--- resources/views/emails/templates/welcome-old.blade.php
+++ resources/views/emails/templates/welcome-old.blade.php
@@ -1,74 +1,131 @@
-@extends('emails.master_layout')
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+ <head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta name="x-apple-disable-message-reformatting">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta content="telephone=no" name="format-detection">
+  <title>Intégration 2023</title><!--[if (mso 16)]>
+    <style type="text/css">
+    a {text-decoration: none;}
+    </style>
+    <![endif]--><!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--><!--[if gte mso 9]>
+<xml>
+    <o:OfficeDocumentSettings>
+    <o:AllowPNG></o:AllowPNG>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+  <style type="text/css">
+#outlook a {
+	padding:0;
+}
+.es-button {
+	mso-style-priority:100!important;
+	text-decoration:none!important;
+}
+a[x-apple-data-detectors] {
+	color:inherit!important;
+	text-decoration:none!important;
+	font-size:inherit!important;
+	font-family:inherit!important;
+	font-weight:inherit!important;
+	line-height:inherit!important;
+}
+.es-desk-hidden {
+	display:none;
+	float:left;
+	overflow:hidden;
+	width:0;
+	max-height:0;
+	line-height:0;
+	mso-hide:all;
+}
+[data-ogsb] .es-button {
+	border-width:0!important;
+	padding:10px 20px 10px 20px!important;
+}
+[data-ogsb] .es-button.es-button-1 {
+	padding:10px 20px!important;
+}
+@media only screen and (max-width:600px) {p, ul li, ol li, a { line-height:150%!important } h1, h2, h3, h1 a, h2 a, h3 a { line-height:120%!important } h1 { font-size:30px!important; text-align:center } h2 { font-size:26px!important; text-align:center } h3 { font-size:20px!important; text-align:center } .es-header-body h1 a, .es-content-body h1 a, .es-footer-body h1 a { font-size:30px!important } .es-header-body h2 a, .es-content-body h2 a, .es-footer-body h2 a { font-size:26px!important } .es-header-body h3 a, .es-content-body h3 a, .es-footer-body h3 a { font-size:20px!important } .es-menu td a { font-size:16px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px!important } .es-content-body p, .es-content-body ul li, .es-content-body ol li, .es-content-body a { font-size:16px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:16px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class="gmail-fix"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } a.es-button, button.es-button { font-size:20px!important; display:block!important; border-width:10px 0px 10px 0px!important } .es-adaptive table, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:600px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } tr.es-desk-hidden { display:table-row!important } table.es-desk-hidden { display:table!important } td.es-desk-menu-hidden { display:table-cell!important } .es-menu td { width:1%!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } }
+</style>
+ </head>
+ <body style="width:100%;font-family:arial, 'helvetica neue', helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
+  <div class="es-wrapper-color" style="background-color:#F6F6F6"><!--[if gte mso 9]>
+			<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+				<v:fill type="tile" color="#f6f6f6"></v:fill>
+			</v:background>
+		<![endif]-->
+   <table cellpadding="0" cellspacing="0" class="es-wrapper" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top">
+     <tr>
+      <td valign="top" style="padding:0;Margin:0">
+       <table cellpadding="0" cellspacing="0" class="es-content" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
+         <tr>
+          <td align="center" style="padding:0;Margin:0">
+           <table bgcolor="#e74260" class="es-content-body" align="center" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#e74260;width:600px">
+             <tr>
+              <td align="left" bgcolor="#E74260" style="padding:0;Margin:0;padding-left:20px;padding-right:20px;background-color:#e74260">
+               <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                 <tr>
+                  <td align="center" valign="top" style="padding:0;Margin:0;width:560px">
+                   <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                     <tr>
+                      <td align="center" style="padding:0;Margin:0;font-size:0px"><img class="adapt-img" src="https://i.imgur.com/C3iVEdr.jpg" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="560"></td>
+                     </tr>
+                   </table></td>
+                 </tr>
+               </table></td>
+             </tr>
+                      
+             <tr>
+              <td align="left" bgcolor="#ffffff" style="padding:0;Margin:0;padding-top:15px;padding-left:20px;padding-right:20px;background-color:#ffffff">
+               <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                 <tr>
+                  <td align="center" valign="top" style="padding:0;Margin:0;width:560px">
+                   <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                     <tr>
+                      <td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:helvetica, 'helvetica neue', arial, verdana, sans-serif;line-height:27px;color:#E74260;font-size:18px"><strong>Cahier de vacances</strong><strong></strong></p></td>
+                     </tr>
+                     <tr>
+                      <td style="padding:0;Margin:0">
+                        <p style="margin: 20; -webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; font-family: roboto, 'helvetica neue', helvetica, arial, sans-serif; line-height: 24px; color: #e8425f; font-size: 16px;">Salut toi !<br><br>
 
-@section('title')
-Bienvenue à l'UTT
-@endsection
-
-@section('content')
-
-<table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">
-  <tbody class="mcnTextBlockOuter">
-    <tr>
-      <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
-        <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
-          <tbody>
-            <tr>
-              <td valign="top" class="mcnTextContent" style="padding: 0px 18px 9px;color: #222222;font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, Verdana, sans-serif;font-size: 14px;line-height: 150%;text-align: justify;">
-                <h1 style="text-align: center;">
-                  <span style="color:#E74360">
-                    <span style="font-family:merriweather sans,helvetica neue,helvetica,arial,sans-serif">
-                      <strong>
-                        <span style="font-size:20px">Salut {{ $user->first_name }}</span>
-                      </strong>
-                    </span>
-                  </span>
-                </h1>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </td>
-    </tr>
-  </tbody>
-</table>
-<table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">
-  <tbody class="mcnTextBlockOuter">
-    <tr>
-      <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
-        <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer">
-          <tbody>
-            <tr>
-              <td valign="top" class="mcnTextContent" style="padding: 0px 18px 9px;color: #222222;font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, Verdana, sans-serif;font-size: 14px;line-height: 150%;text-align: justify;">
-                <div style="text-align: justify;">
-                  <span style="font-family:helvetica neue,helvetica,arial,verdana,sans-serif">
-                    <span style="color:#696969">
-                      <span style="font-size:14px">Bravo pour ton acceptation à l'UTT !</span>
-                    </span>
-                  </span>
-                </div>
-                <div style="text-align: justify;">
-                  <br>
-                  <span style="font-size:14px">
-                    <span style="color:#696969">
-                      Nous sommes l'équipe d'intégration, des étudiants bénévoles qui préparons minutieusement ton arrivée pour que celle-ci reste inoubliable.<br>
-                      &nbsp;<br>
-                      Un tas d'événements incroyables, tous basés sur la base du volontariat, t'attendent dès le {{ Config::get('services.reentry.tc.date') }} si tu arrives en première année et dès le {{ Config::get('services.reentry.branches.date') }} si tu arrives en 3ème année ou en master. Tout est fait pour que tu t'éclates et que tu rencontres les personnes qui feront de ton passage à l'UTT un moment inoubliable.
-                    </span><br>
-                    &nbsp;<br>
-                    <span style="color:#696969">Mais avant toutes choses il faut te préparer. Assure-toi de réaliser les tâches suivantes avant ton arrivée :</span>
-                  </span>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </td>
-    </tr>
-  </tbody>
-</table>
-
-@include('emails.parts.connect')
-<!--@include('emails.parts.godfather')-->
-@include('emails.parts.covid')
-
-@endsection
+                            Ta rentrée se rapproche (doucement t'inquiète), et histoire que tu ne sois pas perdu quand les cours vont reprendre, on t'a concocté des <strong>petites activités</strong> à faire avant la rentrée !<br>
+                            Pour être au niveau, on t'invite à remplir minutieusement ce cahier de vacances que tu peux télécharger juste <strong><a href="https://drive.google.com/file/d/1ymRlzAnM_gEiekYE3TlM1X2DDyDhjH7Y/view?usp=sharing" target="_blank" style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; text-decoration: underline; color: #e8425f; font-size: 16px;" rel="noopener">ici</a></strong>. Une fois fini, envoie le à l'adresse <strong><a href="mailto:integration+cahier@utt.fr" target="_blank" style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; text-decoration: underline; color: #e8425f; font-size: 16px;" rel="noopener">integration+cahier@utt.fr</a></strong> avant la rentrée.<br><br>
+                            
+                            Petit conseil pour le remplir : tu peux utiliser le site <strong><a href="https://www.ilovepdf.com/fr/modifier-pdf" target="_blank" style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; text-decoration: underline; color: #e8425f; font-size: 16px;" rel="noopener">https://www.ilovepdf.com/fr/modifier-pdf</a></strong>
+                        </p>
+                     </td>
+                     </tr>
+                     
+                   </table></td>
+                 </tr>
+               </table></td>
+             </tr>
+             
+             <tr>
+              <td align="left" style="Margin:0;padding-bottom:15px;padding-top:20px;padding-left:20px;padding-right:20px">
+               <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                 <tr>
+                  <td align="center" valign="top" style="padding:0;Margin:0;width:560px">
+                   <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                     <tr>
+                        <td align="center" style="padding:0;Margin:0;font-size:0px">
+                            <p style="margin: 0; -webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; font-family: roboto, 'helvetica neue', helvetica, arial, sans-serif; line-height: 24px; color: #ffffff; font-size: 16px;">On a <strong>h&acirc;te de te voir</strong> &agrave; la rentr&eacute;e et en attendant, <strong>bonnes vacances</strong>&nbsp;:)<br /><br />Pour <strong>rester au courant de toutes les actualit&eacute;s </strong>de l'Int&eacute;gration, rejoins le <strong><a href="https://discord.gg/Ea8XwgX5HS" target="_blank" style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; text-decoration: underline; color: #ffffff; font-size: 16px;" rel="noopener">serveur Discord de l'Int&eacute;gration</a></strong>,&nbsp;le <strong><a href="https://integration.utt.fr/" target="_blank" style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; text-decoration: underline; color: #ffffff; font-size: 16px;" rel="noopener">site de l'Int&eacute;gration</a></strong>, le <strong><a href="https://www.instagram.com/bdeutt/" target="_blank" style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; text-decoration: underline; color: #ffffff; font-size: 16px;" rel="noopener">compte Instagram du BDE</a>&nbsp;</strong>et le&nbsp;<strong><a href="https://www.facebook.com/bde.utt/" target="_blank" style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; text-decoration: underline; color: #ffffff; font-size: 16px;" rel="noopener">compte Facebook du BDE</a></strong>&nbsp;.
+                            </p>
+                        </td>
+                   </table></td>
+                 </tr>
+               </table></td>
+             </tr>
+           </table></td>
+         </tr>
+       </table></td>
+     </tr>
+   </table>
+  </div>
+ </body>
+</html>
